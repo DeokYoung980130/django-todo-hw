8c444438cceb1129e8badccbc253ebaa9fb45597
deokyoung98 <didejrdud23@naver.com>
2023-09-07 10:58:38 +0900

    post

--- tweet/views.py
+++ tweet/views.py
@@ -1,6 +1,6 @@
 # tweet/views.py
 from django.shortcuts import render, redirect
-
+from .models import TweetModel
 
 # Create your views here.
 def home(request):
@@ -18,3 +18,10 @@ def tweet(request):
             return render(request, 'tweet/home.html')
         else:
             return redirect('/sign-in')
+    elif request.method == 'POST':
+        user = request.user
+        my_tweet = TweetModel()
+        my_tweet.author =user
+        my_tweet.content = request.POST.get('my-content', '')
+        my_tweet.save()
+        return redirect('/tweet')
